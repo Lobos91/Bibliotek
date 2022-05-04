b902259d6c7a0c4dd14ed501608848b906aca3e2
MorrisNumminen <Morris.fr.numminen@gmail.com>
2022-05-04 14:57:44 +0200

    Added all pages
    Added all pages exept for login and registration. That includes the SeachPage , addItemPage, rapportPage and the addAdminPage.

---
+++ Bibliotek/Pages/rapport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bibliotek.Pages
+{
+    public class rapportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
