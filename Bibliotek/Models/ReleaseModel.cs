0e36d3a845996b15d1b4d42a0a94bba52da430ea
MorrisNumminen <Morris.fr.numminen@gmail.com>
2022-05-17 09:16:42 +0200

    Addad ReleseModel

---
+++ Bibliotek/Models/ReleaseModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bibliotek.Models
+{
+    public class ReleaseModel
+    {
+        public int ID { get; set; } 
+        public string? Title { get; set; }
+        public string Genre { get; set; }   
+
+        [DisplayFormat(ApplyFormatInEditMode = true)]
+        public DateTime? LoanDateTimeStart { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true)]
+        public DateTime? LoanDateTimeEnd { get; set; }
+    }
+}
