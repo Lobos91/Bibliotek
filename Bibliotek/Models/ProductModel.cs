252da3706f94fbea71537749d75db1e23614e91f
MorrisNumminen <Morris.fr.numminen@gmail.com>
2022-05-23 11:37:09 +0200

    changed models

---
+++ Bibliotek/Models/ProductModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bibliotek.Models
+{
+    public class ProductModel
+    {
+        public bool Lent { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true)]
+        public DateTime? LoanDateTimeStart { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true)]
+        public DateTime? LoanDateTimeEnd { get; set; }
+
+        public class Book : ProductModel
+        {
+            public string Author { get; set; }
+            public int? Pages { get; set; }
+        }
+
+        public class EBook : ProductModel
+        {
+            public string Author { get; set; }
+            public int? Pages { get; set; }
+            public string Type { get; set; } // PDF, EPUB, MOBI, IBA etc
+        }
+
+        public class Movie : ProductModel
+        {
+            public int? Length { get; set; }
+            public string Director { get; set; }
+        }
+    }
+}
--- Bibliotek/Models/ReleaseModel.cs
+++ Bibliotek/Models/ReleaseModel.cs
@@ -1,36 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace Bibliotek.Models
 {
     public class ReleaseModel
     {
-        public int ID { get; set; } 
+        public int ID { get; set; }
         public string? Title { get; set; }
-        public string Genre { get; set; }   
-        public bool Lent { get; set; }
+        public string Genre { get; set; }
 
-        [DisplayFormat(ApplyFormatInEditMode = true)]
-        public DateTime? LoanDateTimeStart { get; set; }
-        [DisplayFormat(ApplyFormatInEditMode = true)]
-        public DateTime? LoanDateTimeEnd { get; set; }
-    }
-
-    public class Book : ReleaseModel
-    {
-        public string Author { get; set; }
-        public int? Pages { get; set; }
-    }
-
-    public class EBook : ReleaseModel
-    {
-        public string Author { get; set; }
-        public int? Pages { get; set; }
-        public string Type { get; set; } // PDF, EPUB, MOBI, IBA etc
-    }
-
-    public class Movie : ReleaseModel
-    {
-        public int? Length { get; set; }
-        public string Director { get; set; }
     }
 }
